9811d03395a05d9f537e818d36a553a5f029a3f9
ayamkv <65036385+ayamkv@users.noreply.github.com>
2024-07-11 00:05:42 +0700

    -

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -55,10 +55,10 @@ export const serializeNonPOJOs = (obj) => {
     return structuredClone(obj);
 }
 
-export const generateUsername = (name) => {
-    const id = randomBytes(2).toString('hex')
-    return `${name.slice(0, 5)}${id}`
-}
+// export const generateUsername = (name) => {
+//     const id = randomBytes(2).toString('hex')
+//     return `${name.slice(0, 5)}${id}`
+// }
 
 // export const getImageURL = (collectionId, recordId, fileName, size = '0x0') => {
 //     return `${publicUrl}/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`;
