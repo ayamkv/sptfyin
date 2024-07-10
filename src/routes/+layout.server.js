fc74cf4ce18da499ce1965cf72eac69d60845197
ayamkv <65036385+ayamkv@users.noreply.github.com>
2024-07-10 23:54:40 +0700

    -

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,28 +1,28 @@
-import { serializeNonPOJOs } from "$lib/utils.js"
-import { error } from "@sveltejs/kit"
+// import { serializeNonPOJOs } from "$lib/utils.js"
+// import { error } from "@sveltejs/kit"
 
 
 
-export const load = ({ locals, url }) => {
-    let userTypeRes = serializeNonPOJOs(locals.user?.collectionName)
+// export const load = ({ locals, url }) => {
+//     let userTypeRes = serializeNonPOJOs(locals.user?.collectionName)
     
 
 
-    if (locals.user)  {
-        if (userTypeRes === 'users') {
-            console.log('user')
-        }
+//     if (locals.user)  {
+//         if (userTypeRes === 'users') {
+//             console.log('user')
+//         }
 
-        return {
-            user: locals.user,
-            userType: userTypeRes,
-            currentPath: url.pathname,
-        }
-    };
+//         return {
+//             user: locals.user,
+//             userType: userTypeRes,
+//             currentPath: url.pathname,
+//         }
+//     };
 
-    return {
-        user: undefined,
-        currentPath: url.pathname
-    }
+//     return {
+//         user: undefined,
+//         currentPath: url.pathname
+//     }
 
-}
+// }
