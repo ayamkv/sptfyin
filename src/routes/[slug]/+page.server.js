2b221143a3a73c4f8ff69ff4a3a9420fd9023788
ayamkv <65036385+ayamkv@users.noreply.github.com>
2025-04-25 20:21:09 +0700

    might fix cloudflare funky thinga

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -93,6 +93,6 @@ export const load = async ({ params, request }) => {
         throw error(404, 'Link does not exist, but may be available in the future. <br>yeehaw 🔍🤠');
     }
     
-    throw redirect(303, link);
+    throw redirect(301, link);
 };
 
