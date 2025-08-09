2fd2edc5defcf3d3f8c348aa1ccd1735cf4fec05
ayamkv <65036385+ayamkv@users.noreply.github.com>
2025-08-09 21:19:57 +0700

    chore(adapter): switch to @sveltejs/adapter-cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+        adapter: cloudflare(),
 		csrf: {
             checkOrigin: false
 		},
