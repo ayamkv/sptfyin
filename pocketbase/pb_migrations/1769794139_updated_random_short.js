6193dc80b3a8a43d376507f9002a9cd5b35f2f21
ayamkv <65036385+ayamkv@users.noreply.github.com>
2026-01-31 21:59:02 +0700

    chore: normalize line endings and add beads setup

---
+++ pocketbase/pb_migrations/1769794139_updated_random_short.js
@@ -0,0 +1,45 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate(
+	(app) => {
+		const collection = app.findCollectionByNameOrId('ocypvwxnoapmygg');
+
+		// update field
+		collection.fields.addAt(
+			1,
+			new Field({
+				exceptDomains: null,
+				hidden: false,
+				id: 'uc5eiwqv',
+				name: 'from',
+				onlyDomains: ['spotify.com', 'open.spotify.com', 'spotify.link'],
+				presentable: false,
+				required: true,
+				system: false,
+				type: 'url'
+			})
+		);
+
+		return app.save(collection);
+	},
+	(app) => {
+		const collection = app.findCollectionByNameOrId('ocypvwxnoapmygg');
+
+		// update field
+		collection.fields.addAt(
+			1,
+			new Field({
+				exceptDomains: null,
+				hidden: false,
+				id: 'uc5eiwqv',
+				name: 'from',
+				onlyDomains: ['spotify.com', 'open.spotify.com'],
+				presentable: false,
+				required: true,
+				system: false,
+				type: 'url'
+			})
+		);
+
+		return app.save(collection);
+	}
+);
