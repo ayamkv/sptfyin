f5ee7692fa5143b5369ae976e581b61a457b718a
ayamkv <65036385+ayamkv@users.noreply.github.com>
2026-02-18 05:35:08 +0700

    style: add ghost3 button variant and update border radius to rounded-xl

--- src/lib/components/ui/toggle/toggle.svelte
+++ src/lib/components/ui/toggle/toggle.svelte
@@ -2,11 +2,12 @@
 	import { tv } from 'tailwind-variants';
 
 	export const toggleVariants = tv({
-		base: 'ring-offset-background hover:text-muted-foreground focus-visible:ring-ring data-[state=on]:text-accent-foreground inline-flex items-center justify-center gap-2 rounded-md text-sm font-thin transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+		base: 'ring-offset-background hover:text-muted-foreground focus-visible:ring-ring data-[state=on]:text-accent-foreground inline-flex items-center justify-center gap-2 rounded-xl text-sm font-thin transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
 		variants: {
 			variant: {
 				default: 'bg-transparent hover:bg-muted data-[state=on]:bg-accent',
 				outline: 'border-input hover:bg-accent hover:text-accent-foreground border bg-transparent',
+				ghost: 'border border-secondary/20 shadow-md hover:bg-accent hover:text-accent-foreground hover:bg-secondary/80 hover:outline-primary hover:inverseShadow active:scale-95 transition-all data-[state=on]:text-accent-foreground data-[state=on]:bg-background/30 data-[state=on]:inverseShadow',
 				ghost2:
 					'bg-gradient-to-br from-[#302D3A] via-5% text-secondary-foreground hover:text-accent-foreground hover:bg-secondary/40 hover:inverseShadow highlightSecondary active:scale-95 transition-all data-[state=on]:text-accent-foreground data-[state=on]:bg-background/30 data-[state=on]:inverseShadow border-t'
 			},
